c7414468a0f465b88932af7afb47dda93908d490
Per Halvor Tryggeseth <per.halvor@gmail.com>
2014-10-02 19:03:33 +0200

    README

--- index.js
+++ index.js
@@ -10,12 +10,28 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
 app.get('/', function(req,res) {
     res.render('home');
 });
 
-app.get('/about', function(req,res) {
-    res.render('about', {fortune: fortune.getFortune()});
+app.get('/about', function (req, res) {
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    });
+});
+
+app.get('/tours/hood-river', function(req,res) {
+    res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req,res) {
+    res.render('tours/request-group-rate');
 });
 
 app.get('/about/contact', function(req,res) {
---
+++ public/qa/tests-global.js
@@ -0,0 +1,5 @@
+suite('Global tests', function() {
+    test('page has a valid title', function(){
+        assert(document.title && document.title.match(/\S/) && document.title.toUpperCase() !== 'TODO');
+    });
+});
